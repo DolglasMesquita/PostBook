9bbdcf20733faa6aade61c06b50f00b9d028632a
dolglas_mesquita@hotmail.com <dolglas_mesquita@hotmail.com>
2023-11-02 13:43:59 -0300

    update

--- Program.cs
+++ Program.cs
@@ -59,10 +59,13 @@ builder.Services.AddAuthorization(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+}
 
-    app.UseSwagger();
-    app.UseSwaggerUI();
-
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+c.SwaggerEndpoint("/swagger/v1/swagger.json", "PostBook v1"));
 
 app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 
