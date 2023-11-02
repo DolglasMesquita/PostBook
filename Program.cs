6c8b9250567a7b0a3f0440e0a2ce709a7eb248f8
dolglas_mesquita@hotmail.com <dolglas_mesquita@hotmail.com>
2023-11-02 13:32:23 -0300

    update database

--- Program.cs
+++ Program.cs
@@ -15,7 +15,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<PostBookContext>(options => options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDbContext<PostBookContext>(options => options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"), new MySqlServerVersion(new Version(10, 4, 21))));
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 
