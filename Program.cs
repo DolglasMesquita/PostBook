988624274ac6cd7a7626870e4027dadc3d8c7f87
dolglas_mesquita@hotmail.com <dolglas_mesquita@hotmail.com>
2023-11-02 03:27:35 -0300

    update

--- Program.cs
+++ Program.cs
@@ -15,7 +15,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<PostBookContext>(options => options.UseSqlite(builder.Configuration.GetConnectionString("Data Source=PostBook.db")));
+builder.Services.AddDbContext<PostBookContext>(options => options.UseSqlite("Data Source=PostBook.db"));
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 
